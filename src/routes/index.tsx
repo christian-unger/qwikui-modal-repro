4b847d352e121ca099d27956dcfa4c8b134669a2
christian unger <54417601+christian-unger@users.noreply.github.com>
2025-01-10 19:46:31 -0500

    cleaned up a bit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,75 +8,38 @@ import {
 } from "@builder.io/qwik";
 import { Modal } from "@qwik-ui/headless";
 
-/**
- * Example:
- * - Modal works as intended when not conditionally rendered.
- * - Modal does not work when conditionally rendered.
- *
- * - Minimal version of our component included below for reference.
- */
-
 export default component$(() => {
-    const notConditionallyRenderedIsOpenSignal = useSignal(false);
-    const conditionallyRenderedIsOpenSignal = useSignal(false);
+    const isOpenSignal = useSignal(false);
+    const inputTextSignal = useSignal("");
     return (
         <>
-            {/* Not Conditionally Rendered: */}
-
             <button
                 onClick$={() => {
-                    notConditionallyRenderedIsOpenSignal.value = true;
+                    isOpenSignal.value = true;
                 }}
             >
-                Open Modal (Not Conditionally Rendered)
+                Open Modal
             </button>
             <QwikUiModal
                 title="Hello"
-                isOpenSignal={notConditionallyRenderedIsOpenSignal}
+                isOpenSignal={isOpenSignal}
                 closeOnBackdropClick={true}
             >
                 <div>Modal content:</div>
+                <input type="text" bind:value={inputTextSignal} />
                 {Array.from({ length: 10 }, (_, i) => (
                     <div key={i}>Content {i}</div>
                 ))}
             </QwikUiModal>
-
-            <br />
-            <br />
-            <br />
-
-            {/* Conditionally Rendered: */}
-
-            <button
-                onClick$={() => {
-                    conditionallyRenderedIsOpenSignal.value = true;
-                }}
-            >
-                Open Modal (Conditionally Rendered)
-            </button>
-            {conditionallyRenderedIsOpenSignal.value === true && (
-                <QwikUiModal
-                    title="Hello"
-                    isOpenSignal={conditionallyRenderedIsOpenSignal}
-                    closeOnBackdropClick={true}
-                >
-                    <div>Modal content:</div>
-                    {Array.from({ length: 10 }, (_, i) => (
-                        <div key={i}>Content {i}</div>
-                    ))}
-                </QwikUiModal>
-            )}
         </>
     );
 });
 
 const QwikUiModal = component$(
     (props: {
-        title: string | JSXOutput;
         isOpenSignal: Signal<boolean>;
+        title: string | JSXOutput;
         description?: string | JSXOutput;
-        onClickClose$?: QRL<() => void>;
-        onClickOutside$?: QRL<() => void>;
         closeOnBackdropClick?: boolean;
     }) => {
         return (
@@ -85,29 +48,26 @@ const QwikUiModal = component$(
                 closeOnBackdropClick={props.closeOnBackdropClick}
             >
                 <Modal.Panel>
-                    <header>
-                        <div>
-                            <Modal.Title>{props.title}</Modal.Title>
-                            <button
-                                class="unstyled"
-                                autoFocus={false}
-                                onClick$={() => {
-                                    props.onClickClose$?.();
-                                    props.isOpenSignal.value = false;
-                                }}
-                            >
-                                ✕
-                            </button>
-                        </div>
-                        {props.description && (
-                            <Modal.Description>
-                                {props.description}
-                            </Modal.Description>
-                        )}
-                    </header>
-                    <section>
-                        <Slot />
-                    </section>
+                    {props.isOpenSignal.value === true && (
+                        <>
+                            <header>
+                                <Modal.Title>{props.title}</Modal.Title>
+                                <button
+                                    onClick$={() => {
+                                        props.isOpenSignal.value = false;
+                                    }}
+                                >
+                                    close
+                                </button>
+                                <Modal.Description>
+                                    {props.description}
+                                </Modal.Description>
+                            </header>
+                            <section>
+                                <Slot />
+                            </section>
+                        </>
+                    )}
                 </Modal.Panel>
             </Modal.Root>
         );
